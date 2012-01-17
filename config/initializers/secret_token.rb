a545aba5a3102776cc9556ca52c855d0fe471ac0
Bartłomiej Matla <apxe.king.expa@gmail.com>
2012-01-17 23:17:04 +0100

    Inicjacja

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App2::Application.config.secret_token = 'b65f3844e26bc580824268b076b3ac46a1eaa93eb2aa12665faf4752746270ed432734887b9ed90a1a34846878383653b771ec99f9b33ff356651141232370fe'
